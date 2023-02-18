175931f18e2adb529edb04ead5d3556405414ec7
rovxxmjxx <miju3545@gmail.com>
2023-02-18 22:18:49 +0900

    feat: auth view - signup, login, forgot

---
+++ src/components/auth/ForgotPasswordView/ForgotPasswordView.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react'
+
+const ForgetPasswordView: FC = () => {
+  return <div>ForgetPasswordView</div>
+}
+
+export default ForgetPasswordView
---
+++ src/components/auth/index.ts
@@ -0,0 +1,3 @@
+export { LoginView } from './LoginView'
+export { SignUpView } from './SignUpView'
+export { ForgotPasswordView } from './ForgotPasswordView'
