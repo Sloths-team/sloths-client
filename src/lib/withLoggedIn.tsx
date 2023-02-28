43eb18699104cb385032665f9bb64db15c78b03a
rovxxmjxx <miju3545@gmail.com>
2023-02-28 18:22:49 +0900

    feat: session - router 분리

---
+++ src/lib/withLoggedIn.tsx
@@ -0,0 +1,51 @@
+import { ComponentType, useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { useSession } from '../components/common/Layout/context'
+
+type Props = {}
+
+export const withLoggedIn = <P extends Props>(
+  WrappedComponent: ComponentType<P>
+) => {
+  const Component = (props: P) => {
+    const { isUserLoggedIn } = useSession()
+    const router = useRouter()
+
+    useEffect(() => {
+      if (!isUserLoggedIn) {
+        router.push('/login')
+      }
+    }, [isUserLoggedIn])
+
+    return <WrappedComponent {...props} />
+  }
+
+  Object.keys(WrappedComponent).forEach(
+    (k) => ((Component as any)[k] = (WrappedComponent as any)[k])
+  )
+
+  return Component
+}
+
+export const withNotLoggedIn = <P extends Props>(
+  WrappedComponent: ComponentType<P>
+) => {
+  const Component = (props: P) => {
+    const { isUserLoggedIn } = useSession()
+    const router = useRouter()
+
+    useEffect(() => {
+      if (isUserLoggedIn) {
+        router.back()
+      }
+    }, [isUserLoggedIn])
+
+    return <WrappedComponent {...props} />
+  }
+
+  Object.keys(WrappedComponent).forEach(
+    (k) => ((Component as any)[k] = (WrappedComponent as any)[k])
+  )
+
+  return Component
+}
