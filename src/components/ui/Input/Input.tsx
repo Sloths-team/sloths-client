9c07c786f1e90e269ddb4638b49e1208061bea08
rovxxmjxx <miju3545@gmail.com>
2023-02-28 18:23:40 +0900

    refactor: ui - Input

---
+++ src/components/ui/Input/Input.tsx
@@ -0,0 +1,44 @@
+import React, { FC, InputHTMLAttributes, ChangeEvent } from 'react'
+import { Control, Controller } from 'react-hook-form'
+
+export type Props = InputHTMLAttributes<HTMLInputElement> & {
+  type?: 'email' | 'password' | 'number' | 'text'
+  name: string
+  control: Control<any>
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void
+}
+
+const Input: FC<Props> = (props) => {
+  const {
+    id,
+    type = 'text',
+    value,
+    name,
+    control,
+    onChange = () => {},
+    ...rest
+  } = props
+
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field }) => (
+        <input
+          type={type}
+          autoComplete="off"
+          autoCorrect="true"
+          autoCapitalize="off"
+          spellCheck="false"
+          {...field}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => {
+            onChange(e)
+            field.onChange(e.target.value)
+          }}
+          {...rest}
+        />
+      )}
+    />
+  )
+}
+export default Input
