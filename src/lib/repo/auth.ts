8ca4cf5100b9bb1351c4de4194609669f5852edf
rovxxmjxx <miju3545@gmail.com>
2023-03-03 23:16:18 +0900

    feat: repo 분리

---
+++ src/lib/repo/auth.ts
@@ -0,0 +1,28 @@
+import { fetcher } from '@lib/queryClient'
+
+export type LoginBody = {
+  email: string
+  password: string
+}
+
+export type SignUpBody = {
+  name: string
+  email: string
+  password: string
+}
+
+export const login = async ({ email, password }: LoginBody) => {
+  return await fetcher({
+    method: 'POST',
+    path: '/api/auth/login',
+    body: { email, password },
+  })
+}
+
+export const signUp = async ({ name, email, password }: SignUpBody) => {
+  return await fetcher({
+    method: 'POST',
+    path: '/api/auth/signup',
+    body: { name, email, password },
+  })
+}
