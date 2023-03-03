d26b60e96e6f6c3ac53a718a0df54e52a0aee3e2
rovxxmjxx <miju3545@gmail.com>
2023-03-03 22:36:50 +0900

    chore

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,22 +4,27 @@ import type { AppProps } from 'next/app'
 import { FC } from 'react'
 import { Head } from '@components/common'
 import { ManagedUIContext } from '@components/ui/context'
+import { getClient } from '../lib/queryClient'
+import { QueryClientProvider } from 'react-query'
 
 const Noop: FC<{ children?: React.ReactNode }> = ({ children }) => (
   <>{children}</>
 )
 
 export default function App({ Component, pageProps }: AppProps) {
+  const client = getClient()
   const Layout = (Component as any).Layout || Noop
 
   return (
     <>
       <Head />
-      <ManagedUIContext>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ManagedUIContext>
+      <QueryClientProvider client={client}>
+        <ManagedUIContext>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ManagedUIContext>
+      </QueryClientProvider>
     </>
   )
 }
