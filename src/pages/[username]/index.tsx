919e88abd9de7995f2011d2b930b3cb48c7cae4a
rovxxmjxx <miju3545@gmail.com>
2023-03-08 21:51:05 +0900

    feat: 404, profile, projects page

---
+++ src/pages/[username]/index.tsx
@@ -0,0 +1,9 @@
+import { Layout } from '@components/common'
+
+const Profile = () => {
+  return <div>프로필</div>
+}
+
+Profile.Layout = Layout
+
+export default Profile
---
+++ src/pages/projects/new.tsx
@@ -0,0 +1,10 @@
+import CreateProjectView from '@components/project/CreateProjectView'
+import { Layout } from '@components/common'
+
+const CreateProject = () => {
+  return <CreateProjectView />
+}
+
+CreateProject.Layout = Layout
+
+export default CreateProject
