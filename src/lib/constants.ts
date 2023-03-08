f394e0871c191b719134951768a9eb1d5a253320
rovxxmjxx <miju3545@gmail.com>
2023-03-08 21:50:44 +0900

    refactor: constants 분리

---
+++ src/lib/constants.ts
@@ -0,0 +1,10 @@
+export const AUTH_TOKEN_KEY = 'authorization'
+export const QUERY_KEYS = {
+  LOGIN: 'LOGIN',
+  SIGNUP: 'SIGNUP',
+  LOGOUT: 'LOGOUT',
+  ACCESS_TOKEN: 'ACCESS_TOKEN',
+} as const
+
+export const BASE_URL = 'http://localhost:8080'
+export const GITHUB_BASE_URL = ''
