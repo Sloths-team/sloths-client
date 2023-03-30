6de311dca1fbce1c26792b6d5572604d6f601bee
rovxxmjxx <miju3545@gmail.com>
2023-03-31 00:30:04 +0900

    feat: /me/profile - undone

--- src/components/me/Profile/ProfileView.tsx
+++ src/components/me/Profile/ProfileView.tsx
@@ -17,43 +17,10 @@ const useInput = (
   return [value, onChange]
 }
 
-const ToggleInput = ({
-  title,
-  defaultValue,
-  onSubmit,
-}: {
-  title: string
-  defaultValue: string
-  onSubmit: () => void
-}) => {
-  const ref = useRef<HTMLInputElement>(null)
-  const [value, onChange] = useInput(defaultValue)
-  const [disabled, toggleDisabled] = useReducer((p) => !p, true)
-
-  useEffect(() => {
-    ref?.current?.focus()
-  }, [ref])
-
-  return (
-    <div className={s.toggle}>
-      <input ref={ref} value={value} onChange={onChange} disabled={disabled} />
-      <div className={s.actions}>
-        {disabled && <RoundButton onClick={toggleDisabled}>변경</RoundButton>}
-        {!disabled && (
-          <>
-            <RoundButton onClick={toggleDisabled}>취소</RoundButton>
-            <RoundButton onClick={onSubmit} colored>
-              인증 {title}보내기
-            </RoundButton>
-          </>
-        )}
-      </div>
-    </div>
-  )
-}
-
 const ProfileView: FC = () => {
   const router = useRouter()
+  const [nickname, setNickname] = useState('')
+  const [bio, setBio] = useState('')
 
   return (
     <div className={s.root}>
@@ -62,16 +29,26 @@ const ProfileView: FC = () => {
         <div className={s.container}>
           <div className={s.wrapper}>
             <span className={s.label}>닉네임</span>
-            <input />
+            <input
+              value={nickname}
+              onChange={(e) => setNickname(e.target.value)}
+            />
           </div>
           <div className={s.wrapper}>
             <span className={s.label}>소개</span>
-            <textarea />
+            <textarea value={bio} onChange={(e) => setBio(e.target.value)} />
           </div>
         </div>
         <div className={s.actions}>
-          <RoundButton>취소하기</RoundButton>
-          <RoundButton colored>저장하기</RoundButton>
+          <RoundButton
+            type="button"
+            onClick={() => router.push('/me/settings')}
+          >
+            취소하기
+          </RoundButton>
+          <RoundButton type="button" onClick={() => {}} colored>
+            저장하기
+          </RoundButton>
         </div>
       </form>
     </div>
